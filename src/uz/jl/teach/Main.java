285b16969aa71f43aa9d047cefe557901fc5dc9c
jlkesh <john.lgd65@gmail.com>
2022-11-20 15:22:41 +0500

    initial

--- src/uz/jl/teach/Main.java
+++ src/uz/jl/teach/Main.java
@@ -1,7 +1,45 @@
 package uz.jl.teach;
 
+/**
+ * this class just for testing
+ * @author jlkesh
+ */
 public class Main {
+
+    public Main() {
+    }
+
+    /**
+     * this is entry point method
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    /**
+     * This method multiplies given double parameter by 2
+     *
+     * @param a is input parameter
+     * @return returns doubled value of <code>a</code> input param
+     * @see uz.jl.teach.Main#multiplyByTwo(int)
+     */
+    public double multiplyByTwo(double a) {
+        return 2 * a;
+    }
+
+
+    /**
+     * {@index entry}
+     * This method multiplies given int parameter by 2
+     * {@link  uz.jl.teach.Main#multiplyByTwo(double) }
+     *
+     * @param a is input parameter
+     * @return returns doubled value of <code>a</code> input param
+     */
+    public int multiplyByTwo(int a) {
+        return 2 * a;
+    }
+
 }
---
+++ src/uz/jl/teach/Test.java
@@ -0,0 +1,17 @@
+package uz.jl.teach;
+
+
+/**
+ * @author jlkesh
+ */
+public class Test {
+    /**
+     * this is entry point method for
+     * @see uz.jl.teach.Test Test
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.multiplyByTwo(12D);
+    }
+}
