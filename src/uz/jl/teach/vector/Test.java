612a6cf37285ee3628648955a27a4ed34e4442b0
jlkesh <john.lgd65@gmail.com>
2022-11-21 15:24:37 +0500

    initial

---
+++ src/uz/jl/teach/vector/Test.java
@@ -0,0 +1,15 @@
+package uz.jl.teach.vector;
+
+import java.util.Stack;
+import java.util.Vector;
+
+public class Test {
+    public static void main(String[] args) {
+        Vector<String> strings = new Vector<>();
+        new Stack<String>();
+    }
+}
+
+
+
+
